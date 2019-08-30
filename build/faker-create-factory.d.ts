fa32fe3e023b7d975e55eadcfb27053701bb397e
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2019-08-30 18:04:14 -0300

    chore: Remove build folder

--- build/faker-create-factory.d.ts
+++
@@ -1,5 +0,0 @@
-import * as faker from 'faker';
-export declare function createFactory<T = any>(schemaGenerator: (fakerInstance: typeof faker) => T): {
-    generateMany: (ammount: number) => T[];
-    generate: () => T;
-};
